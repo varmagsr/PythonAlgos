dadaa0a39852d612e835bc8dad7bd2abd4ea258c
Rajesh varma <varmagsr@gmail.com>
2022-08-29 00:28:26 +0530

    Add files via upload

---
+++ BinarySearch.py
@@ -0,0 +1,7 @@
+
+
+#Binary Search
+arr1 = [10, 20, 30, 40, 45, 44, 78, 87, 90]
+num = 44
+print(arr1.index(num))
+
---
+++ BubbleSort.py
@@ -0,0 +1,26 @@
+#Bubble Sort
+def bubbleSort(theSeq):
+    n = len(theSeq)
+
+    for i in range(n - 1):
+        flag = 0
+
+        for j in range(n - 1):
+
+            if theSeq[j] > theSeq[j + 1]:
+                tmp = theSeq[j]
+                theSeq[j] = theSeq[j + 1]
+                theSeq[j + 1] = tmp
+                flag = 1
+
+        if flag == 0:
+            break
+
+    return theSeq
+
+
+el = [21, 62, 91, 313, 13]
+
+result = bubbleSort(el)
+
+print(result)
